70feb18ba7693a8063007af5a15922c532338582
Nobuo Kato <katonobu4649@gmail.com>
2021-11-28 22:12:40 +0900

    Updates

---
+++ component---src-pages-404-js-d4e7564be5793dd3e669.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[883],{9616:function(e,t,l){l.r(t),l.d(t,{default:function(){return r}});var n=l(7294);function r(){return n.createElement(n.Fragment,null,n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"ページが見つかりませんでした。"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-d4e7564be5793dd3e669.js.map
